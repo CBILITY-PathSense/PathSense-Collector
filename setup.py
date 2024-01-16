9c686967a7c96cf0ffd04eee6aecebee6a37bb4b
JiraPit <pitakwongjira@gmail.com>
2024-01-16 15:15:13 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,21 @@ import argparse
 
 def install():
     subprocess.run(["chmod", "+x", "setup_sh/install.sh"])
-    subprocess.run(["sudo setup_sh/install.sh"])
+    subprocess.run(["cd", "setup_sh"])
+    subprocess.run(["sudo install.sh"])
+    subprocess.run(["cd",".."])
 
 def update():
     subprocess.run(["chmod", "+x", "setup_sh/update.sh"])
-    subprocess.run(["sudo setup_sh/update.sh"])
+    subprocess.run(["cd", "setup_sh"])
+    subprocess.run(["sudo update.sh"])
+    subprocess.run(["cd",".."])
 
 def remove_from_boot():
     subprocess.run(["chmod", "+x", "setup_sh/remove-from-boot.sh"])
-    subprocess.run(["sudo setup_sh/remove-from-boot.sh"])
+    subprocess.run(["cd", "setup_sh"])
+    subprocess.run(["sudo remove-from-boot.sh"])
+    subprocess.run(["cd",".."])
 
 def main(command):
     if command == "install": install()
