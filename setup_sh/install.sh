3ef28210e2f0bcca07e56f3ccd6befa633e1500e
JiraPit <pitakwongjira@gmail.com>
2024-01-16 15:49:33 +0700

    Update install.sh

--- setup_sh/install.sh
+++ setup_sh/install.sh
@@ -18,12 +18,12 @@ sudo cp -r CollectorScript /
 echo "PathSense collector script added to root directory"
 
 # Check if main.py is already in the boot file
-if grep -Fxq "cd /CollectorScript && python main.py &" /etc/rc.local
+if grep -Fxq "cd /CollectorScript && sudo python main.py &" /etc/rc.local
 then
     echo "PathSense collector script is already set to run at boot"
 else
     # If not, add the command to the boot file
-    sed -i '$i cd /CollectorScript && python main.py &' /etc/rc.local
+    sed -i '$i cd /CollectorScript && sudo python main.py &' /etc/rc.local
     echo "PathSense collector script is now set to run at boot"
 fi
 
