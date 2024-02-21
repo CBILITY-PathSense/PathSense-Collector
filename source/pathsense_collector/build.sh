31c4486570f70c50e90f725f24c39ae404261820
Jira Pit <pitakwongjira@gmail.com>
2024-02-21 23:37:52 +0700

    02/21

--- source/pathsense_collector/build.sh
+++ source/pathsense_collector/build.sh
@@ -1,4 +1,11 @@
 rm -d -r ../bin
 mkdir -p ../bin
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install \
+    build-essential \
+    libopencv-dev \
+    libclang-dev \
+    clang
 cargo build --release
 cp target/release/pathsense_collector ../bin/pathsense_collector
