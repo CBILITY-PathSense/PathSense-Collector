4c6ea7296bda9c100efdc0818c0bc8f61c9a4822
Jira Pit <pitakwongjira@gmail.com>
2024-02-28 10:24:52 +0700

    Run in Background

--- sh/install.sh
+++ sh/install.sh
@@ -15,11 +15,11 @@ sudo cp -r bin/* /pathsense-collector/
 echo "PathSense-collector files is copied to /pathsense-collector"
 
 # Check if pathsense-collector is already in the boot file
-if grep -Fxq "cd /pathsense-collector && ./pathsense_collector" /etc/rc.local; then
+if grep -Fxq "cd /pathsense-collector && ./pathsense_collector &" /etc/rc.local; then
     echo "PathSense-collector is already set to run at boot"
 else
     # If not, add the command to the boot file
-    sudo sed -i -e '$i \cd /pathsense-collector && ./pathsense_collector\n' /etc/rc.local
+    sudo sed -i -e '$i \cd /pathsense-collector && ./pathsense_collector &\n' /etc/rc.local
     echo "PathSense-collector is now set to run at boot"
 fi
 
--- sh/remove.sh
+++ sh/remove.sh
@@ -7,9 +7,9 @@ sudo rm -d -r /pathsense-collector
 echo "PathSense-collector files is removed"
 
 # Check if pathsense-collector is in boot
-if grep -Fxq "cd /pathsense-collector && ./pathsense_collector" /etc/rc.local; then
+if grep -Fxq "cd /pathsense-collector && ./pathsense_collector &" /etc/rc.local; then
     # Remove pathsense-collector from boot
-    sudo sed -i '/cd /pathsense-collector && .\/pathsense_collector/d' /etc/rc.local
+    sudo sed -i '/cd /pathsense-collector && .\/pathsense_collector &/d' /etc/rc.local
     echo "PathSense-collector is removed from boot"
 fi
 
