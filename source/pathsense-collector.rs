8021dfa7c3f7583a93152f0b500ab8a73e859d44
Jira Pit <pitakwongjira@gmail.com>
2024-08-21 18:53:53 +0700

    refactor

--- source/pathsense-collector.rs
+++ source/pathsense-collector.rs
@@ -9,13 +9,13 @@ fn run_command(command: &str) {
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        eprintln!("Invalid number of arguments. Please use install or remove");
+        eprintln!("Invalid number of arguments. Please use install or remove.");
         return;
     }
 
     match args[1].as_str() {
         "install" => run_command("sh/install.sh"),
         "remove" => run_command("sh/remove.sh"),
-        _ => eprintln!("Invalid command. Please use install or remove"),
+        _ => eprintln!("Invalid command. Please use install or remove."),
     }
 }
